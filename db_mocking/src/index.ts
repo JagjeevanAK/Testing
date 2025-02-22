a607a8fe280232e4d4ca011462db00bfe170a4eb
Jagjeevan <83103469+JagjeevanAK@users.noreply.github.com>
2025-02-22 23:35:53 +0530

    Testing CI CD pipline

--- db_mocking/src/index.ts
+++ db_mocking/src/index.ts
@@ -26,7 +26,7 @@ app.post('/sum',async (req,res): Promise<any>=>{
             type: RequestType.sum
         }
     })
-    res.status(200).json({ result: ans, id: request.a });
+    res.status(200).json({ result: ans, id: request.id });
 })
 
 app.post('/multiply',async (req,res): Promise<any>=>{
